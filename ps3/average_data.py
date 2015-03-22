2b4611000e1bca9c7445ceffbeecac0c7a32ac6b
James Bailey <james.bailey@gmail.com>
2015-03-22 16:49:40 -0400

    Adding PS3 readme and some missing files.

---
+++ ps3/average_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('data_files', nargs='+',
+                           help='an integer for the accumulator')
+
+args = parser.parse_args()
+num_files = len(args.data_files)
+sum_data = None
+for path in args.data_files:
+    with open(path) as f:
+        probs = f.readline().split()
+        data = [float(x) for x in f.readline().split()]
+        if sum_data is None:
+            sum_data = [0]*len(data)
+        sum_data = [ sum(x) for x in zip(data, sum_data) ]
+
+avg_data = [x/float(num_files) for x in sum_data]
+print " ".join(probs)
+print " ".join([str(x) for x in avg_data])
