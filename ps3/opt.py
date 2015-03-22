fab75ac5de78512a0186e6d0dad09b1225bbc884
James Bailey <james.bailey@gmail.com>
2015-03-22 02:59:53 -0400

    Cleaned up optimization program.

--- ps3/opt.py
+++ ps3/opt.py
@@ -4,66 +4,86 @@ from pulp import *
 
 #rho_exp = [5., 6., 7.]
 
-def optimize(rho):
-    def try_threshold(epsilon, rho):
-        rho_exp = [rho]
+class LDPC_opt:
+    def __init__(self):
+        self.rho_exp = [5]
+        self.l_min = 2
+        self.l_max = 100
+        self.m = 40
+        
+        self.lambdas = None
 
-        def rho(x):
-            return sum([(1./float(len(rho_exp)))*x**exp for exp in rho_exp])
-        
+    def rho(self, x):
+        return sum([(1./float(len(self.rho_exp)))*x**exp for exp in self.rho_exp])
+
+    def optimize_lambda(self, epsilon):
+        self.lambdas = dict()
+        for i in range(self.l_min,self.l_max+1):
+            self.lambdas[i] = LpVariable("lambda_%02d"%i,0,1)
+
         prob = LpProblem("Erasure threshold maximization",LpMaximize)
+        prob += sum([l/float(i) for i, l in self.lambdas.iteritems()])
         
-        l_min = 2
-        l_max = 40
+        for j in range(1,self.m+1):
+            x_j = float(j) * epsilon / float(self.m)
+            prob += sum([self.lambdas[i]*epsilon*(1-self.rho(1-x_j))**(float(i)-1) for i,l in self.lambdas.iteritems()]) <= x_j, "threshold %d"%j 
         
-        lambdas = dict()
-        for i in range(l_min,l_max+1):
-            lambdas[i] = LpVariable("lambda_%02d"%i,0,1)
-        
-        #print lambdas
-        prob += sum([l/float(i) for i, l in lambdas.iteritems()])
-        
-        m = 40  # divide continuous f(e,x) < x  into 100 discrete constraints
-        
-        for j in range(1,m+1):
-            x_j = float(j) * epsilon / float(m)
-            prob += sum([lambdas[i]*epsilon*(1-rho(1-x_j))**(float(i)-1) for i,l in lambdas.iteritems()]) <= x_j, "threshold %d"%j 
-        
-        prob += epsilon*lambdas[2] * sum(rho_exp)/float(len(rho_exp)) <= 1.
-        #print prob
-        prob += sum(lambdas.values()) == 1.
-        
+        prob += epsilon*self.lambdas[2] * sum(self.rho_exp)/float(len(self.rho_exp)) <= 1.
+        prob += sum(self.lambdas.values()) == 1.
         
         prob.solve()
-        #print "Status:", LpStatus[prob.status]
+
+    def optimize_for_rate(self, target_rate):
+        self.l_max = 100
+        t_min = 0.3
+        t_max = 0.6
+        while True:
+            t = (t_max+t_min)/2.
+            ensemble.optimize_lambda(t)
+            design_rate = ensemble.design_rate
+            #print design_rate
+
+            # dynamic l_max optimizer
+            l_biggest = 0
+            for i,l in ensemble.lambdas.iteritems():
+                if l.varValue > 0:
+                    l_biggest = max(l_biggest, i)
+
+            if abs(design_rate-target_rate) <1E-4 and self.l_max != l_biggest:
+                break
+            if design_rate < target_rate:
+                t_max = t
+            else:
+                t_min = t
+
+            self.l_max = int((self.l_max + l_biggest)/2. + 1)
+        return t
         
-        
-        
-        design_rate = 1 - sum([(1/float(len(rho_exp)))/(float(i)+1) for i in rho_exp])/sum([l.varValue/float(i) for i,l in lambdas.iteritems()])
-        return design_rate, lambdas
+    @property
+    def design_rate(self):
+        design_rate = 1 - sum([(1/float(len(self.rho_exp)))/(float(i)+1) for i in self.rho_exp])/sum([l.varValue/float(i) for i,l in self.lambdas.iteritems()])
+        return design_rate
 
-    t_min = 0.3
-    t_max = 0.6
-    r_final = 0.5
-    for i in range(20):
-        t = (t_max+t_min)/2.
-        (rate, lambdas) = try_threshold(t, rho)
-        print rate
-        if abs(rate-r_final) <1E-4:
-            break
-        if rate < r_final:
-            #print "decrease t"
-            t_max = t
-        else:
-            #print "increase t"
-            t_min = t
-
-    
-    for i,l in lambdas.iteritems():
-        if l.varValue > 1E-6:
-            print l.name, "=", l.varValue
-    print "design rate: %f" % rate
-    print "epsilon: %f" % t
+    def print_info(self):
+        for i,l in self.lambdas.iteritems():
+            if l.varValue > 0:
+                print l.name, "=", l.varValue
+        print "design rate: %f" % self.design_rate
 
 
-optimize(6)
+ensemble = LDPC_opt()
+
+ensemble.rho_exp = [5]
+t = ensemble.optimize_for_rate(0.5)
+ensemble.print_info()
+print "epsilon: %f" % t
+
+ensemble.rho_exp = [6]
+t = ensemble.optimize_for_rate(0.5)
+ensemble.print_info()
+print "epsilon: %f" % t
+
+ensemble.rho_exp = [7]
+t = ensemble.optimize_for_rate(0.5)
+ensemble.print_info()
+print "epsilon: %f" % t
