28c288148ec04d5b1a51fed806d5f042a0036e5e
James Bailey <james.bailey@gmail.com>
2015-03-22 18:36:53 -0400

    Adding latex rendering to plots.  Adding pdf plots.

--- ps3/plotResults.py
+++ ps3/plotResults.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 import glob
 from scipy import interpolate
+
+matplotlib.rc('font', family='Computer Modern') 
+matplotlib.rc('text', usetex=True)
 
 def running_mean(x, N):
     weights = np.repeat(1.0, N)/N
@@ -30,7 +34,7 @@ def plot_show(name, x_label, legend=None):
     plt.grid(b=True, which='major', color='grey', linestyle='--')
     plt.grid(b=True, which='minor', color='grey', linestyle='--')
     if legend:
-        plt.legend(title=legend, loc="lower left")
+        plt.legend(title=None, loc="lower left")
     plt.title(name)
     plt.xlabel(x_label)
     plt.ylabel("Bit error rate")
@@ -42,17 +46,17 @@ def plot_dir(dir):
         plot_file(path, label="c1, r=5")
 
 plot_dir("results/r5")
-plot_show(name="Irregular LDPC, r=5", x_label="Erasure Probability")
+plot_show(name=r'$LDPC(1200, \lambda_{opt1}(x), x^5)$', x_label="Erasure Probability")
 
 plot_dir("results/r6")
-plot_show(name="Irregular LDPC, r=6", x_label="Erasure Probability")
+plot_show(name=r'$LDPC(1200, \lambda_{opt2}(x), x^6)$', x_label="Erasure Probability")
 
 plot_dir("results/r7")
-plot_show(name="Irregular LDPC, r=7", x_label="Erasure Probability")
+plot_show(name=r'$LDPC(1200, \lambda_{opt3}(x), x^7)$', x_label="Erasure Probability")
 
-plot_file("results/avg_reg.data", label="Regular")
-plot_file("results/r5_avg.data", label="r=5")
-plot_file("results/r6_avg.data", label="r=6")
-plot_file("results/r7_avg.data", label="r=7")
+plot_file("results/avg_reg.data", label=r'$LDPC(1200, x^2, x^5)$')
+plot_file("results/r5_avg.data", label=r'$LDPC(1200, \lambda_{opt1}(x), x^5)$')
+plot_file("results/r6_avg.data", label=r'$LDPC(1200, \lambda_{opt2}(x), x^6)$')
+plot_file("results/r7_avg.data", label=r'$LDPC(1200, \lambda_{opt3}(x), x^7)$')
 plt.ylim([10E-6,1E0])
 plot_show(name="Performance over 10 random codes", x_label="Erasure Probability", legend="Ensemble")
