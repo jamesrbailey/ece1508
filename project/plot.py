c04ec483e08890cad5f5120dc04753138f16a60b
James Bailey <james.bailey@gmail.com>
2015-05-04 09:18:49 -0400

    Adding n2500 window results.

--- project/plot.py
+++ project/plot.py
@@ -53,3 +53,10 @@ plot_file("results/m1024/wd_w8_l64.data", label=r'$w=8$')
 plt.ylim([1E-5,1E0])
 plot_show(name="Window Decoder Performance of SC-LDPC", x_label="Erasure Probability", legend="Decoder")
 
+plot_file("results/m2500/wd_w1_l64.data", label=r'$w=1$')
+plot_file("results/m2500/wd_w2_l64.data", label=r'$w=2$')
+plot_file("results/m2500/wd_w4_l64.data", label=r'$w=4$')
+plot_file("results/m2500/wd_w8_l64.data", label=r'$w=8$')
+plt.ylim([1E-5,1E0])
+plot_show(name="Window Decoder Performance of SC-LDPC, $n=2500$", x_label="Erasure Probability", legend="Decoder")
+
